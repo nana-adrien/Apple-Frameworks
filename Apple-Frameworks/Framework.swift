865b0a2f48d5f35c6b01ae02e233e91ce06add5b
adrien nana <kakonana7@gmail.com>
2026-02-11 03:45:54 +0100

    project initialization

---
+++ Apple-Frameworks/Framework.swift
@@ -0,0 +1,18 @@
+//
+//  Framework.swift
+//  Apple-Frameworks
+//
+//  Created by Digiprem on 11/02/2026.
+//
+
+import SwiftUI
+
+struct Framework: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Framework()
+}
---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Digiprem on 11/02/2026.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
